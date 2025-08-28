9d1c0f21f7baa7388e59814d14130888e980209f
claes <claes@holmerson.net>
2025-08-28 22:42:19 +0200

    Bump nixpkgs version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS module to manage secrets outside nix store";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = {
     self,
